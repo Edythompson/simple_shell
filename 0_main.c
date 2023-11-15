0a75139391d71b65c707688831625e77c61fb54f
Edythompson <edyythompson@gmail.com>
2023-11-14 21:27:42 -0800

    betty compliance

---
+++ 0_main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int sum;
+int y;
+int x;
+y = 0;
+x = 0;
+sum = y + x;
+printf("sum is %d\n", sum);
+return (0);
+}
---
+++ beticompi.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+/**
+peticompi - program that is betty compliance
+* @y: number to be added
+* @x: second number to be added
+* @sum: sum of numbers
+* Return: void
+*/
+int beticompi(int y, int x)
+{
+int sum;
+y = 4;
+x = 5;
+sum = y + x;
+printf("sum is %d\n", sum);
+return (0);
+} 
